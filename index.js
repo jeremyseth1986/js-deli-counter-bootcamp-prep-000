72d03309b3040d402ab30e7bc9878e95ddd0b0f8
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-02 21:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function currentLine(line) {
   while ( i < katzDeliLine.length) i++; {
     katzDeliLine.push(" " +[i+1]+". "  + katzDeliLine[i])
   }
-  if (katzDeliLine === 0) {
+  if (line.length === 0) {
     return "The line is currently empty.";
   } else
-  return(`The line is currently:` + line);
+  return(`The line is currently:` + katzDeliLine);
 }
